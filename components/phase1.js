0317eb0242ec8f2a61eaf3cb5cb0b9c965621798
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2024-04-03 20:46:32 -0500

    se modularizo el codigo creando mas clase para los distintos niveles

---
+++ components/phase1.js
@@ -0,0 +1,34 @@
+import { Phase } from './phase.js'
+
+export class Phase1 extends Phase {
+
+  create() {
+
+    this.bricks = this.relatedScene.physics.add.staticGroup({
+      key: ['bluebrick', 'orangebrick', 'greenbrick', 'blackbrick', 'yellowbrick', 'blackbrick', 'yellowbrick', 'bluebrick', 'orangebrick', 'greenbrick'],
+      frameQuantity: 1,
+      gridAlign: {
+        width: 5,
+        height: 4,
+        cellWidth: 150,
+        cellHeight: 100,
+        x: 135,
+        y: 150
+      }
+    
+    });
+
+    this.fixedBricks = this.relatedScene.physics.add.staticGroup();
+    this.fixedBricks.create(316, 165, 'greybrick');
+    this.fixedBricks.create(466, 165, 'greybrick');
+
+    this.configureColisions();
+    this.configureColisionsFixed();
+  }
+
+  
+}
+
+
+
+
